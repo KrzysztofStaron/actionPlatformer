0aadfc603f6c721ea55af26ed98c1f1f2cd3d3d6
Krzystof Staroń <kisiel3141@gmail.com>
2022-04-10 19:26:26 +0200

    player

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,23 @@
 extends KinematicBody2D
 
-export var speed : int = 80
+var velocity := Vector2.ZERO
+
+export var maxSpeed :float= 70
+export var step := 210
+
+export var gravitation := 180
+export var maxGravitation := 270
+
+export var jumpHeight := -90
 
 func _ready() -> void:
-	pass
+	if OS.is_debug_build():
+		print("speed change: ", maxSpeed/step, "sec")
+
+func _process(delta: float) -> void:
+	var dir := MoveInput.axis("move_left", "move_right")
+
+	velocity.y = move_toward(velocity.y, maxGravitation, gravitation * delta)
+	velocity.x = move_toward(velocity.x, maxSpeed * dir, step * delta)
+
+	velocity = move_and_slide(velocity)
