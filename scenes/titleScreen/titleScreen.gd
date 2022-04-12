0b04e51fdf7330afe67667bf5f787a025acd1cab
Krzystof Staroń <kisiel3141@gmail.com>
2022-04-12 15:39:33 +0200

    woringstart game

---
+++ scenes/titleScreen/titleScreen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_Exit_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_Options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_Start_pressed() -> void:
+	print(get_tree().change_scene("res://game.tscn"))
