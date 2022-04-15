20b7a2c63154fc0d8c3549afe16f106ba2ed8a03
Krzystof Staroń <kisiel3141@gmail.com>
2022-04-15 19:18:10 +0200

    add quiting

--- scenes/titleScreen/titleScreen.gd
+++ scenes/titleScreen/titleScreen.gd
@@ -1,10 +1,11 @@
 extends Control
 
 func _on_Exit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
 
 func _on_Options_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_Start_pressed() -> void:
-	print(get_tree().change_scene("res://game.tscn"))
+	# warning-ignore: RETURN_VALUE_DISCARDED
+	get_tree().change_scene("res://scenes/game.tscn")
