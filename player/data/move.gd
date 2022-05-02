e33578d6ea778b40089941e253821db3556fd91b
Krzystof Staroń <kisiel3141@gmail.com>
2022-05-02 16:50:09 +0200

    separate move

--- player/data/move.gd
+++ player/data/move.gd
@@ -1,4 +1,45 @@
 extends Node
 
+var gravitation : int
+var maxGravitation : int
+
+var velocity := Vector2.ZERO
+var grounded := true
+onready var player := get_parent().get_parent()
+
 export var maxSpeed : float
 export var step : int
+
+export var dust : PackedScene
+
+export var jumpHeight := -90
+
+export var speedMultiplayer := 1.0
+
+func _ready() -> void:
+	var settings = preload("res://settings.gd")
+
+	gravitation = settings.gravitation
+	maxGravitation = settings.maxGravitation
+
+func _physics_process(delta: float) -> void:
+	var dir := MoveInput.axis("move_left", "move_right")
+
+	velocity.y = move_toward(velocity.y, maxGravitation, gravitation * delta)
+	velocity.x = move_toward(velocity.x, maxSpeed * dir * speedMultiplayer, step * delta)
+
+	velocity = player.move_and_slide(velocity)
+
+func _input(event) -> void:
+	if event.is_action_pressed("jump") and grounded:
+		velocity.y = jumpHeight
+		summon_dust()
+
+func _on_ground_state_changed(_body:Node) -> void:
+	var bodies = player.get_node("groundSensor").get_overlapping_bodies()
+	grounded = bodies.size() == 1
+
+func summon_dust():
+	var newDust := dust.instance()
+	get_parent().add_child(newDust)
+	newDust.set_global_position(player.get_global_position() + Vector2(0, 9))
--- player/player.gd
+++ player/player.gd
@@ -2,51 +2,16 @@ extends KinematicBody2D
 
 class_name Player
 
-var velocity := Vector2.ZERO
-
-var maxSpeed : float
-var step : float
-
-export var speedMultiplayer := 1.0
-
-var gravitation : int
-var maxGravitation : int
-
-export var jumpHeight := -90
 export var autoJump := 0.15
-
-export var dust : PackedScene
-
-var atackDamage : float
-var atackRandomines : float
-var knockback : float
 
 export var maxHealth := 100
 var health : int
 
 onready var animation = $animation.get("parameters/playback")
 
-var grounded := true
-
 func _ready() -> void:
 	$animation.active = true
 	health = maxHealth
-	var settings = preload("res://settings.gd")
-
-	gravitation = settings.gravitation
-	maxGravitation = settings.maxGravitation
-
-	var moveData := $data/move
-	maxSpeed = moveData.maxSpeed
-	step = moveData.step
-	
-func _physics_process(delta: float) -> void:
-	var dir := MoveInput.axis("move_left", "move_right")
-
-	velocity.y = move_toward(velocity.y, maxGravitation, gravitation * delta)
-	velocity.x = move_toward(velocity.x, maxSpeed * dir * speedMultiplayer, step * delta)
-
-	velocity = move_and_slide(velocity)
 
 func _process(_delta: float) -> void:
 	if health <= 0:
@@ -72,9 +37,6 @@ func _process(_delta: float) -> void:
 func _input(event) -> void:
 	if event.is_action_pressed("swordAtack"):
 		animation.travel("swordAtack")
-	elif event.is_action_pressed("jump") and grounded:
-		velocity.y = jumpHeight
-		summon_dust()
 
 func die():
 	get_tree().paused = true
@@ -84,16 +46,7 @@ func die():
 	set_process(false)
 	set_physics_process(false)
 
-func summon_dust():
-	var newDust := dust.instance()
-	get_parent().add_child(newDust)
-	newDust.set_global_position(get_global_position() + Vector2(0, 9))
-
 func takeDamage(damage : int):
 	$hitAim.play("hit")
 	health -= damage
 	print(damage)
-
-func _on_ground_state_changed(_body:Node) -> void:
-	var bodies = $groundSensor.get_overlapping_bodies()
-	grounded = bodies.size() == 1
