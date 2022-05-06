cfd20fb1cc61e883a9d73137cfbc531f86a5a360
Krzystof Staroń <kisiel3141@gmail.com>
2022-05-06 20:25:14 +0200

    working platforms

--- map/platform.gd
+++ map/platform.gd
@@ -1,5 +1,18 @@
 extends CollisionPolygon2D
+onready var timer := Timer.new()
+	
+func _ready() -> void:
+	timer.wait_time = 0.2
+	timer.connect("timeout", self, "_on_timer_timeout") 
+	add_child(timer)
 
-
-func _process(delta: float) -> void:
-	disabled = Input.is_action_pressed("move_down")
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("move_down"):
+		disabled = true
+		timer.wait_time = 0.2
+		timer.start()
+		
+func _on_timer_timeout():
+	disabled = false
+	timer.stop()
+	timer.wait_time = 0.2
